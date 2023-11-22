710f0ba4cd5b0941767e5960741a3d89776d191a
Datta-070 <chinmaydatt003@gmail.com>
2023-11-22 17:46:14 +0530

    Final Adjustments made

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+let rs=document.getElementById('rs');
+let no=document.getElementById('no');
 function split(){
-    let rs=document.getElementById('rs');
-    let no=document.getElementById('no');
+    if(rs.value > 0){
     let res= (rs.value/no.value).toFixed(2);
     document.getElementById('head').innerText=res;
+    }
 }
